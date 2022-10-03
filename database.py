8477ef8280b51a45ef02d82a4786b4728c390e6e
AbishekThirumurugan <114929463+AbishekThirumurugan@users.noreply.github.com>
2022-10-03 19:13:59 +0530

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="Task@111999",
-  database="ola"
-)
-#connection helps to talk to server whether it is in same machine or not
