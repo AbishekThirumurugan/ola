750cdfd3604ad45156fa6313c6add2d9ace793a7
AbishekThirumurugan <114929463+AbishekThirumurugan@users.noreply.github.com>
2022-10-05 15:26:47 +0530

    Add files via upload

---
+++ config/path.py
@@ -0,0 +1,7 @@
+#url
+home='/'
+create='/signup'
+read='/ola/<string:roles>'
+register='/signin/registrationform'
+delete='/ola/<string:role>' #<parameter> means parameter is passed on to a function
+verify='/signup/otpverification'
